9d920fff4bbc3ce4971b5bd64cb6fe856369c5a5
SparrowXD <tajneyashraj15@gmail.com>
2024-01-05 15:35:54 +0530

    fixed some more errors

--- main.js
+++ main.js
@@ -22,21 +22,21 @@ async function fetchCookies() {
   const database = new MongoClient(`mongodb+srv://admin:${process.env.PASS}@freecluster.7xu0m7g.mongodb.net/?retryWrites=true&w=majority`);
   await database.connect();
 
-  const browser = await puppeteer.launch({
+  try {
+    const browser = await puppeteer.launch({
     headless: "new",
     args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
+        "--disable-setuid-sandbox",
+        "--no-sandbox",
+        "--single-process",
+        "--no-zygote",
     ],
     executablePath:
-      process.env.NODE_ENV === "production"
+        process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
     timeout: 1200000,
-  });
-  try {
+    });
     const page = await browser.newPage();
     await page.goto(
       "https://store.steampowered.com/agecheck/app/1938090/",
@@ -50,9 +50,8 @@ async function fetchCookies() {
     console.log(storedCookies);
     await database.db("steam").collection("cookies").deleteMany({});
     await database.db("steam").collection("cookies").insertOne({_id: "cookies", cookies: storedCookies, timestamp: new Date()});
+    await browser.close();
   } catch (error) {
     console.error(error);
-  } finally {
-    browser.close();
   }
 }
