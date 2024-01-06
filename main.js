641ef88311c3bb9efed4345655ce11c23b2da0d7
SparrowXD <tajneyashraj15@gmail.com>
2024-01-06 20:35:07 +0530

    browser.io

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-const path = require('path');
-require('dotenv').config()
-const chromium = require("chrome-aws-lambda");
+require('dotenv').config();
+const express = require("express");
+const puppeteer = require("puppeteer");
+
+const app = express();
+app.listen(process.env.PORT || 3000, () => {
+    console.log("app online")
+})
 
 async function fetchCookies() {
     try {
-        const chromePath = path.resolve(__dirname, 'chrome.exe')
-        const browser = await chromium.puppeteer.launch({
-            headless: 'new',
-            args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-            defaultViewport: null,
-            executablePath: chromePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
-        })
+        const browser = await puppeteer.connect({ browserWSEndpoint: `wss://chrome.browserless.io?token=${process.env.TOKEN}` });
         const page = await browser.newPage();
         await page.goto(
         "https://store.steampowered.com/agecheck/app/1938090/",
@@ -22,14 +19,15 @@ async function fetchCookies() {
         await page.click("#view_product_page_btn");
         await page.waitForNavigation("https://store.steampowered.com/app/1938090/Call_of_Duty/",{ timeout: 180000 })
         const storedCookies = await page.cookies();
-        console.log(storedCookies);
+        console.log({storedCookies});
         await browser.close();
-        return storedCookies, console.log("Succesfully refreshed cookies");
+        return storedCookies;
     } catch (error) {
         console.error(error);
     }
 }
 
-fetchCookies().then(() => {
-  console.log("test complete");
-});
+app.get("/cookies", async (req, res) => {
+    const response = await fetchCookies();
+    res.send({cookies: response});
+})
