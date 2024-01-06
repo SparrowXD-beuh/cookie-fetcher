b5adaf597ea5ed2f093bf915dedc6ffae0158e71
SparrowXD-beuh <151991041+SparrowXD-beuh@users.noreply.github.com>
2024-01-06 14:18:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,62 +1,109 @@
-const express = require("express");
-const puppeteer = require("puppeteer");
-const { MongoClient } = require("mongodb");
-require('dotenv').config();
+const axios = require("axios");
+const cheerio = require("cheerio");
+const { database } = require("./modules/searchByAppId")
+const headers = require("./public/headers.json");
+require('dotenv').config()
+// const puppeteer = require("puppeteer");
 
-const app = express();
-app.listen(process.env.PORT || 3000, async () => {
-    console.log("app online");
-})
+const chromium = require("chrome-aws-lambda");
 
-app.get("/", (req, res) => {
-    res.send("seems to be working fine.");
-})
+// async function run() {
+//   const response = await axios.get(
+//     `https://store.steampowered.com/agecheck/app/2050650/`,
+//     {
+//       headers: {
+//         headers,
+//       },
+//     }
+//   );
+//   const Headers = response.headers;
+//   if (Headers && Headers["set-cookie"]) {
+//     const cookiesArray = Headers["set-cookie"].map((cookieString) => {
+//       const cookieParts = cookieString.split(";").map((part) => part.trim());
+//       const cookie = {};
+
+//       // Extract name and value
+//       const [name, value] = cookieParts[0].split("=");
+//       cookie.name = name;
+//       cookie.value = value;
+
+//       cookiePath = "/";
+//       cookieParts.slice(1).forEach((part) => {
+//         const [key, val] = part.split("=");
+
+//         if (key.toLowerCase() === "expires") {
+//           cookie.expires = (new Date(val)).getTime();
+//         } else if (key.toLowerCase() === "path") {
+//           cookie.path = val;
+//         } else if (key.toLowerCase() === "domain") {
+//           cookie.domain = val;
+//         } else if (key.toLowerCase() === "secure") {
+//           cookie.secure = true;
+//         } else if (key.toLowerCase() === "httponly") {
+//           cookie.httpOnly = true;
+//         } else if (key.toLowerCase() === "samesite") {
+//           cookie.sameSite = val;
+//         }
+//       });
+//       return cookie;
+//     });
+//     cookiesArray.push({
+//         name: 'wants_mature_content',
+//         value: 1,
+//         path: '/',
+//         secure: true,
+//         sameSite: 'None'
+//     })
+//     console.log(cookiesArray);
+//   }
+// }
+
+fetchCookies().then(() => {
+  console.log("test complete");
+});
 
 async function fetchCookies() {
-  const database = new MongoClient(`mongodb+srv://admin:${process.env.PASS}@freecluster.7xu0m7g.mongodb.net/?retryWrites=true&w=majority`);
-  (database.connect()).then(() => console.log("database connected"));
+    // const database = new MongoClient(`mongodb+srv://admin:${process.env.PASS}@freecluster.7xu0m7g.mongodb.net/?retryWrites=true&w=majority`);
+    // (database.connect()).then(() => console.log("database connected"));
 
-  try {
-    const browser = await puppeteer.launch({
-    headless: "new",
-    args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-    ],
-    executablePath:
-        process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    timeout: 1200000,
-    });
-    const page = await browser.newPage();
-    const domContentLoadedPromise = await new Promise((resolve) => {
-        page.once('domcontentloaded', resolve);
-    });
-    await domContentLoadedPromise;
-    await page.goto(
-      "https://store.steampowered.com/agecheck/app/1938090/",
-      { timeout: 180000 }
-    );
-    await domContentLoadedPromise;
-    await page.select("#ageYear", "1980");
-    await page.click("#view_product_page_btn");
-    await page.waitForNavigation("https://store.steampowered.com/app/1938090/Call_of_Duty/",{ timeout: 180000 })
-    await domContentLoadedPromise;
-    const storedCookies = await page.cookies();
-    console.log(storedCookies);
-    await database.db("steam").collection("cookies").deleteMany({});
-    await database.db("steam").collection("cookies").insertOne({_id: "cookies", cookies: storedCookies, timestamp: new Date()});
-    await browser.close();
-    return storedCookies, console.log("Succesfully refreshed cookies");
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        // const browser = await puppeteer.launch({
+        // headless: "new",
+        // args: [
+            // "--disable-setuid-sandbox",
+            // "--no-sandbox",
+            // "--single-process",
+            // "--no-zygote",
+        // ],
+        // executablePath:
+        //     process.env.NODE_ENV === "production"
+        //     ? process.env.PUPPETEER_EXECUTABLE_PATH
+        //     : puppeteer.executablePath(),
+        // timeout: 1200000,
+        // });
+        const browser = await chromium.puppeteer.launch({
+            headless: false,
+            args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        })
+        const page = await browser.newPage();
+        await page.goto(
+        "https://store.steampowered.com/agecheck/app/1938090/",
+        { timeout: 180000 }
+        );
+        await page.select("#ageYear", "1980");
+        await page.click("#view_product_page_btn");
+        await page.waitForNavigation("https://store.steampowered.com/app/1938090/Call_of_Duty/",{ timeout: 180000 })
+        const storedCookies = await page.cookies();
+        console.log(storedCookies);
+        // await database.db("steam").collection("cookies").deleteMany({});
+        // await database.db("steam").collection("cookies").insertOne({_id: "cookies", cookies: storedCookies, timestamp: new Date()});
+        await browser.close();
+        return storedCookies, console.log("Succesfully refreshed cookies");
+    } catch (error) {
+        console.error(error);
+    }
 }
-
-app.get("/cookies", async (req, res) => {
-    const cookies = await fetchCookies();
-    res.send({cookies: cookies});
-})
